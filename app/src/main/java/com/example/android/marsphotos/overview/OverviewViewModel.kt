fa7dd6baffdca06766200bec171e90dc2e555e7e
rchang1163 <46568041+rchang1163@users.noreply.github.com>
2022-01-29 12:54:40 -0500

    implemented exception handling for when there is no internet connection

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -46,8 +47,12 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsPhotos() {
         viewModelScope.launch {
-            val listResult = MarsApi.retrofitService.getPhotos()
-            _status.value = listResult
+            try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                _status.value = listResult
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
         }
     }
 }
